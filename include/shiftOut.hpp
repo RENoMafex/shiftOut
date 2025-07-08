8cb9b415453ee5f87a1044469f95f4fa26157c1e
Malte Schilling <schilling.malte@googlemail.com>
2025-07-08 22:00:48 +0200

    also removed BitOrder, as its also not supported by all platforms

--- include/shiftOut.hpp
+++ include/shiftOut.hpp
@@ -5,19 +5,19 @@
 
 // bigger shiftOut()
 
-void shiftOut(uint8_t dataPin, uint8_t clockPin, BitOrder bitOrder, uint16_t val);
-void shiftOut(uint8_t dataPin, uint8_t clockPin, BitOrder bitOrder, uint32_t val);
-void shiftOut(uint8_t dataPin, uint8_t clockPin, BitOrder bitOrder, uint64_t val);
+void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint16_t val);
+void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint32_t val);
+void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint64_t val);
 
 // latched shiftOut()
 
-void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t latchPin, BitOrder bitOrder, uint8_t val);
-void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t latchPin, BitOrder bitOrder, uint16_t val);
-void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t latchPin, BitOrder bitOrder, uint32_t val);
-void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t latchPin, BitOrder bitOrder, uint64_t val);
+void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t latchPin, uint8_t bitOrder, uint8_t val);
+void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t latchPin, uint8_t bitOrder, uint16_t val);
+void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t latchPin, uint8_t bitOrder, uint32_t val);
+void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t latchPin, uint8_t bitOrder, uint64_t val);
 
-void shiftOut(uint8_t dataPin, uint8_t clockPin, BitOrder bitOrder, uint8_t vals[], size_t vals_length);
-void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t latchPin, BitOrder bitOrder, uint8_t vals[], size_t vals_length);
+void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t vals[], size_t vals_length);
+void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t latchPin, uint8_t bitOrder, uint8_t vals[], size_t vals_length);
 
 #endif
 
